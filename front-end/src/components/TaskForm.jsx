e0121ec26b27edf8214079edf0e86d1dd6dce624
Enzo Gennari <enzo.gennari02@gmail.com>
2023-09-24 18:40:07 -0300

    Add component TaskForm

---
+++ front-end/src/components/TaskForm.jsx
@@ -0,0 +1,13 @@
+import Button from './Button';
+import Input from './Input';
+
+function TaskForm() {
+  return (
+    <form>
+      <Input />
+      <Button />
+    </form>
+  );
+}
+
+export default TaskForm;
