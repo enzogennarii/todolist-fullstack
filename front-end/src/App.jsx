08a4241279b122194147cbfad0b2f9dbbbdd764b
Enzo Gennari <enzo.gennari02@gmail.com>
2023-09-24 18:40:44 -0300

    Add TaskForm no componente App

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,11 @@
+import TaskForm from './components/TaskForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>TodoList</h1>
+      <TaskForm />
     </div>
   );
 }
